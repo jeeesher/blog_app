35c1dde346d91f813f1d3e5ec43fe569a14d1451
jeeesher <bulaonronanne@gmail.com>
2025-07-26 03:58:23 +0800

    Post associations and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  # Associations
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :title, presence: true
+  validates :body, presence: true
 end
