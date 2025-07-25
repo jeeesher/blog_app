4c3ef7e8bbc842d710c857d118e55b3695362dd5
jeeesher <bulaonronanne@gmail.com>
2025-07-26 03:53:52 +0800

    Made changes on models

--- db/migrate/20250725194222_create_users.rb
+++ db/migrate/20250725194222_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_encrypted
-      t.string :role
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :role, null: false, default: 'user'
 
       t.timestamps
     end
