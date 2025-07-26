77bb6d8c417ca560c7f2f57fe2feae0d180fb9c0
jeeesher <bulaonronanne@gmail.com>
2025-07-26 15:58:39 +0800

    Modified admin/post controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,21 @@
 class Admin::PostsController < ApplicationController
+  before_action :require_admin
+
   def index
+    @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path, notice: "Post deleted successfully."
+  end
+
+  private
+
+  def require_admin
+    unless logged_in? && admin?
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    end
   end
 end
