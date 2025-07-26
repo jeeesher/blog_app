978b049ef4ce54b4b56248c83b9b34bffda73d58
jeeesher <bulaonronanne@gmail.com>
2025-07-26 13:23:13 +0800

    Modified SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully!"
+    redirect_to root_path
   end
 end
