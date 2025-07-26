09df90522b382d3abeb2a49e0673010c0a58342c
jeeesher <bulaonronanne@gmail.com>
2025-07-26 13:22:22 +0800

    Configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  root "posts#index"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :posts do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+  
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
+  namespace :admin do
+    get "dashboard" => "dashboard#index"
+  end
 end
